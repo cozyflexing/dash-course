90cdfd45475cb39947295bcda744c75c11feccf9
Alen Iriskic <cozyflexing@gmail.com>
2022-11-27 00:10:00 +0100

    fk with date

--- app.py
+++ app.py
@@ -5,66 +5,199 @@ from dash_bootstrap_templates import load_figure_template
 from plotly.express import line
 from listCreations import columnOptions, tableOptions, engine
 
-app = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
-load_figure_template("BOOTSTRAP")
-
-GRID_STYLE = {
-    "display": "grid",
-    "height": "1000px",
-    "grid-template-columns": "1fr 4fr",
-    "grid-template-rows": "1fr",
-    "grid-column-gap": "10px",
-    "grid-row-gap": "10px",
+# the style arguments for the sidebar.
+SIDEBAR_STYLE = {
+    "position": "fixed",
+    "top": 0,
+    "left": 0,
+    "bottom": 0,
+    "width": "20%",
+    "padding": "20px 10px",
+    "background-color": "#f8f9fa",
 }
 
-SIDEBAR_STYLE = {
-    "marginLeft": "10px",
-    "marginTop": "10px",
-    "height": "95%",
-    "width": "95%",
-    "padding": "2rem 1rem",
-    "background-color": "#f8f9fa",
+# the style arguments for the main content page.
+CONTENT_STYLE = {
+    "margin-top": "10px",
+    "margin-left": "25%",
+    "margin-right": "5%",
+    "padding": "20px 10p",
+}
+
+TEXT_STYLE = {
+    "textAlign": "center",
+    "color": "black",
+}
+
+CARD_TEXT_STYLE = {
+    "textAlign": "center",
+    "color": "#black",
 }
 
 sidebar = html.Div(
     [
-        html.H2("Sidebar", className="display-4"),
+        html.H2("Menu", style=TEXT_STYLE),
         html.Hr(),
-        html.P("A simple sidebar layout with navigation links", className="lead"),
-        dbc.Nav(
-            [
-                dbc.NavLink("Home", href="/", active="exact"),
-                dbc.NavLink("Page 1", href="/page-1", active="exact"),
-                dbc.NavLink("Page 2", href="/page-2", active="exact"),
-            ],
-            vertical=True,
-            pills=True,
-        ),
     ],
     style=SIDEBAR_STYLE,
 )
 
-app.layout = html.Div(
-    children=[
-        html.Div(
-            children=[sidebar],
+content_first_row = dbc.Row(
+    [
+        dbc.Col(
+            dbc.Card(
+                [
+                    dbc.CardBody(
+                        [
+                            html.H4(
+                                id="card_title_1",
+                                children=["Last:"],
+                                className="card-title",
+                                style=CARD_TEXT_STYLE,
+                            ),
+                            html.P(
+                                id="last",
+                                children=[""],
+                                style=CARD_TEXT_STYLE,
+                            ),
+                        ]
+                    )
+                ]
+            ),
+            md=3,
         ),
-        html.Div(
+        dbc.Col(
+            dbc.Card(
+                [
+                    dbc.CardBody(
+                        [
+                            html.H4(
+                                id="Card Title 2",
+                                children=["Average:"],
+                                className="card-title",
+                                style=CARD_TEXT_STYLE,
+                            ),
+                            html.P(
+                                id="average",
+                                children=[],
+                                style=CARD_TEXT_STYLE,
+                            ),
+                        ]
+                    ),
+                ]
+            ),
+            md=3,
+        ),
+        dbc.Col(
+            dbc.Card(
+                [
+                    dbc.CardBody(
+                        [
+                            html.H4(
+                                id="Card Title 3",
+                                children=["High:"],
+                                className="card-title",
+                                style=CARD_TEXT_STYLE,
+                            ),
+                            html.P(
+                                id="high",
+                                children=[],
+                                style=CARD_TEXT_STYLE,
+                            ),
+                        ]
+                    ),
+                ]
+            ),
+            md=3,
+        ),
+        dbc.Col(
+            dbc.Card(
+                [
+                    dbc.CardBody(
+                        [
+                            html.H4(
+                                id="Card Title 4",
+                                children=["Low:"],
+                                className="card-title",
+                                style=CARD_TEXT_STYLE,
+                            ),
+                            html.P(
+                                id="low",
+                                children=[],
+                                style=CARD_TEXT_STYLE,
+                            ),
+                        ]
+                    ),
+                ]
+            ),
+            md=3,
+        ),
+    ]
+)
+
+content_second_row = dbc.Row(
+    [
+        dbc.Col(dcc.Graph(id="graph_1"), md=4),
+        dbc.Col(dcc.Graph(id="graph_2"), md=4),
+        dbc.Col(dcc.Graph(id="graph_3"), md=4),
+    ]
+)
+
+content_third_row = dbc.Row(
+    [
+        dbc.Col(
             children=[
+                dcc.Graph(id="renderedGraph"),
                 dcc.Dropdown(
                     options=columnOptions, value="OpenInterest", id="columnDropdown"
                 ),
                 dcc.Dropdown(options=tableOptions, value="EUR", id="tableDropdown"),
-                dcc.Graph(id="renderedGraph"),
             ],
-        ),
+            md=12,
+        )
+    ]
+)
+
+content_fourth_row = dbc.Row(
+    [
+        dbc.Col(dcc.Graph(id="graph_5"), md=6),
+        dbc.Col(dcc.Graph(id="graph_6"), md=6),
+    ]
+)
+
+content = html.Div(
+    [
+        html.H2("Sixteen Analytics Dashboard", style=TEXT_STYLE),
+        html.Hr(),
+        content_first_row,
+        # content_second_row,
+        content_third_row,
+        content_fourth_row,
     ],
-    style=GRID_STYLE,
+    style=CONTENT_STYLE,
+)
+
+app = Dash(
+    external_stylesheets=[
+        dbc.themes.BOOTSTRAP,
+    ]
+)
+load_figure_template("BOOTSTRAP")
+
+app.layout = html.Div(
+    [
+        sidebar,
+        content,
+    ]
 )
 
 
 @app.callback(
     Output(component_id="renderedGraph", component_property="figure"),
+    Output(component_id="last", component_property="children"),
+    Output(component_id="average", component_property="children"),
+    Output(component_id="high", component_property="children"),
+    Output(component_id="low", component_property="children"),
     Input(component_id="columnDropdown", component_property="value"),
     Input(component_id="tableDropdown", component_property="value"),
 )
@@ -72,8 +205,14 @@ def renderGraph(selectedColumn, selectedTable):
     df = read_sql(selectedTable, engine)
     df.Date = to_datetime(df.Date)
     lineFig = line(df, x=df.Date, y=selectedColumn, title=f"{selectedColumn}")
-    return lineFig
+    return (
+        lineFig,
+        int(df[selectedColumn].iloc[0]),
+        int(df[selectedColumn].mean()),
+        int(df[selectedColumn].max()),
+        int(df[selectedColumn].min()),
+    )
 
 
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    app.run_server(port="8085", debug=True)
--- test.py
+++ test.py
@@ -1,219 +1,48 @@
-from dash import Dash, html, dcc, Input, Output
-from pandas import to_datetime, read_sql
-import dash_bootstrap_components as dbc
-from dash_bootstrap_templates import load_figure_template
-from plotly.express import line
-from listCreations import columnOptions, tableOptions, engine
+from datetime import date
+from dash import Dash, dcc, html
+from listCreations import engine
+from dash.dependencies import Input, Output
+from pandas import read_sql
 
-import plotly.express as px
+app = Dash(__name__)
 
-# the style arguments for the sidebar.
-SIDEBAR_STYLE = {
-    "position": "fixed",
-    "top": 0,
-    "left": 0,
-    "bottom": 0,
-    "width": "20%",
-    "padding": "20px 10px",
-    "background-color": "#f8f9fa",
-}
+df = read_sql("EUR", engine)
 
-# the style arguments for the main content page.
-CONTENT_STYLE = {
-    "margin-left": "25%",
-    "margin-right": "5%",
-    "padding": "20px 10p",
-}
-
-TEXT_STYLE = {
-    "textAlign": "center",
-    "color": "#191970",
-}
-
-CARD_TEXT_STYLE = {
-    "textAlign": "center",
-    "color": "#0074D9",
-}
-
-sidebar = html.Div(
-    [
-        html.H2("Menu", style=TEXT_STYLE),
-        html.Hr(),
-    ],
-    style=SIDEBAR_STYLE,
-)
-
-content_first_row = dbc.Row(
-    [
-        dbc.Col(
-            dbc.Card(
-                [
-                    dbc.CardBody(
-                        [
-                            html.H4(
-                                id="card_title_1",
-                                children=["Last:"],
-                                className="card-title",
-                                style=CARD_TEXT_STYLE,
-                            ),
-                            html.P(
-                                id="last",
-                                children=[""],
-                                style=CARD_TEXT_STYLE,
-                            ),
-                        ]
-                    )
-                ]
-            ),
-            md=3,
-        ),
-        dbc.Col(
-            dbc.Card(
-                [
-                    dbc.CardBody(
-                        [
-                            html.H4(
-                                id="Card Title 2",
-                                children=["Average:"],
-                                className="card-title",
-                                style=CARD_TEXT_STYLE,
-                            ),
-                            html.P(
-                                id="average",
-                                children=[],
-                                style=CARD_TEXT_STYLE,
-                            ),
-                        ]
-                    ),
-                ]
-            ),
-            md=3,
-        ),
-        dbc.Col(
-            dbc.Card(
-                [
-                    dbc.CardBody(
-                        [
-                            html.H4(
-                                id="Card Title 3",
-                                children=["High:"],
-                                className="card-title",
-                                style=CARD_TEXT_STYLE,
-                            ),
-                            html.P(
-                                id="high",
-                                children=[],
-                                style=CARD_TEXT_STYLE,
-                            ),
-                        ]
-                    ),
-                ]
-            ),
-            md=3,
-        ),
-        dbc.Col(
-            dbc.Card(
-                [
-                    dbc.CardBody(
-                        [
-                            html.H4(
-                                id="Card Title 4",
-                                children=["Low:"],
-                                className="card-title",
-                                style=CARD_TEXT_STYLE,
-                            ),
-                            html.P(
-                                id="low",
-                                children=[],
-                                style=CARD_TEXT_STYLE,
-                            ),
-                        ]
-                    ),
-                ]
-            ),
-            md=3,
-        ),
-    ]
-)
-
-content_second_row = dbc.Row(
-    [
-        dbc.Col(dcc.Graph(id="graph_1"), md=4),
-        dbc.Col(dcc.Graph(id="graph_2"), md=4),
-        dbc.Col(dcc.Graph(id="graph_3"), md=4),
-    ]
-)
-
-content_third_row = dbc.Row(
-    [
-        dbc.Col(
-            children=[
-                dcc.Dropdown(
-                    options=columnOptions, value="OpenInterest", id="columnDropdown"
-                ),
-                dcc.Dropdown(options=tableOptions, value="EUR", id="tableDropdown"),
-                dcc.Graph(id="renderedGraph"),
-            ],
-            md=12,
-        )
-    ]
-)
-
-content_fourth_row = dbc.Row(
-    [
-        dbc.Col(dcc.Graph(id="graph_5"), md=6),
-        dbc.Col(dcc.Graph(id="graph_6"), md=6),
-    ]
-)
-
-content = html.Div(
-    [
-        html.H2("Sixteen Analytics Dashboard", style=TEXT_STYLE),
-        html.Hr(),
-        content_first_row,
-        # content_second_row,
-        content_third_row,
-        content_fourth_row,
-    ],
-    style=CONTENT_STYLE,
-)
-
-app = Dash(
-    external_stylesheets=[
-        dbc.themes.BOOTSTRAP,
-    ]
-)
-load_figure_template("BOOTSTRAP")
 
 app.layout = html.Div(
     [
-        sidebar,
-        content,
+        dcc.DatePickerRange(
+            id="my-date-picker-range",
+            min_date_allowed=date(2000, 1, 1),
+            max_date_allowed=date.today(),
+            initial_visible_month=date.today(),
+            end_date=date.today(),
+        ),
+        html.Div(id="output-container-date-picker-range"),
     ]
 )
 
 
 @app.callback(
-    Output(component_id="renderedGraph", component_property="figure"),
-    Output(component_id="last", component_property="children"),
-    Output(component_id="average", component_property="children"),
-    Output(component_id="high", component_property="children"),
-    Output(component_id="low", component_property="children"),
-    Input(component_id="columnDropdown", component_property="value"),
-    Input(component_id="tableDropdown", component_property="value"),
+    Output("output-container-date-picker-range", "children"),
+    Input("my-date-picker-range", "start_date"),
+    Input("my-date-picker-range", "end_date"),
 )
-def renderGraph(selectedColumn, selectedTable):
-    df = read_sql(selectedTable, engine)
-    df.Date = to_datetime(df.Date)
-    lineFig = line(df, x=df.Date, y=selectedColumn, title=f"{selectedColumn}")
-    return (
-        lineFig,
-        int(df[selectedColumn].iloc[0]),
-        int(df[selectedColumn].mean()),
-        int(df[selectedColumn].max()),
-        int(df[selectedColumn].min()),
-    )
+def update_output(start_date, end_date):
+    string_prefix = "You have selected: "
+    if start_date is not None:
+        start_date_object = date.fromisoformat(start_date)
+        start_date_string = start_date_object.strftime("%D")
+        string_prefix = string_prefix + "Start Date: " + start_date_string + " - "
+    if end_date is not None:
+        end_date_object = date.fromisoformat(end_date)
+        end_date_string = end_date_object.strftime("%B %d, %Y")
+        string_prefix = string_prefix + "End Date: " + end_date_string
+    if len(string_prefix) == len("You have selected: "):
+        return "Select a date to see it displayed here"
+    else:
+        return (string_prefix,)
 
 
 if __name__ == "__main__":
-    app.run_server(port="8085", debug=True)
+    app.run_server(debug=True)
