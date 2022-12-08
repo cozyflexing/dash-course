5525020147dccc98cbc0b8b2be133a6d0f39efd9
cozyflexing <cozyflexing@gmail.com>
2022-12-08 01:38:13 +0100

    some styling bs

--- app.py
+++ app.py
@@ -15,17 +15,20 @@ SIDEBAR_STYLE = {
     "top": 0,
     "left": 0,
     "bottom": 0,
-    "width": "20%",
+    "width": "15%",
     "padding": "20px 10px",
     "background-color": "#f8f9fa",
 }
 
 # the style arguments for the main content page.
 CONTENT_STYLE = {
-    "margin-top": "10px",
-    "margin-left": "25%",
+    "position": "fixed",
+    "top": 0,
+    "bottom": 0,
+    "width": "80%",
+    "margin-left": "17.5%",
     "margin-right": "5%",
-    "padding": "20px 10p",
+    "padding": "20px 10px",
 }
 
 TEXT_STYLE = {
